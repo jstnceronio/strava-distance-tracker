39611099e08e4ee2257cf15dc348414a435eb327
Justin Ceronio <justinceronio@outlook.com>
2024-05-05 17:11:14 +0200

    feat: get weekly kilometers

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import requests
 from dotenv import load_dotenv
+from datetime import date, datetime, time, timedelta
 
 load_dotenv()
 
 auth_url = "https://www.strava.com/oauth/token"
 activities_url = "https://www.strava.com/api/v3/athlete/activities"
-
 payload = {
     'client_id': os.getenv('CLIENT_ID'),
     'client_secret': os.getenv('CLIENT_SECRET'),
@@ -14,12 +14,10 @@ payload = {
     'grant_type': "refresh_token",
     'f': 'json'
 }
-
 print("Requesting Token...\n")
 res = requests.post(auth_url, data=payload, verify=False)
 access_token = res.json()['access_token']
 print("Access Token = {}\n".format(access_token))
-
 
 def summarize_distance_in_list(activities):
     summarized_distance = 0
@@ -30,7 +28,7 @@ def summarize_distance_in_list(activities):
     return summarized_distance
 
 
-def get_all_kilometers():
+def get_all_distance():
     page = 1
     page_not_empty = True
     overall_distance = 0
@@ -50,15 +48,27 @@ def get_all_kilometers():
     return overall_distance
 
 
-print(str(round(get_all_kilometers())) + ' Kilometers')
-
-def get_weekly_kilometers():
-    after_date = ''
-    before_date = ''
+def get_weekly_distance():
+    if date.today().weekday() == 0:
+        last_monday = round(datetime.combine(date.today(), time.min).timestamp())
+    else:
+        last_monday = round(
+            datetime.combine(
+                date.today() - timedelta(days=date.today().weekday()), time.min
+            ).timestamp()
+        )
 
     header = {'Authorization': 'Bearer ' + access_token}
-    param = {'per_page': 200, 'page': 1}  # todo: add weekly parameter
+    param = {'after': last_monday, 'per_page': 200, 'page': 1}  # todo: add weekly parameter
 
     activities = requests.get(activities_url, headers=header, params=param).json()
 
     return summarize_distance_in_list(activities)
+
+
+def main():
+    print(get_weekly_distance())
+
+
+if __name__ == "__main__":
+    main()
