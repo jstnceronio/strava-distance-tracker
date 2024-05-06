29d510edc22c7b574818c816cda6c0a2dd956bdf
Justin Ceronio <justinceronio@outlook.com>
2024-05-06 14:53:00 +0200

    feat: write to display

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os
 import requests
 from dotenv import load_dotenv
 from datetime import date, datetime, time, timedelta
+import RPi.GPIO as GPIO
+from PCF8574.lcd import LCD
+from PCF8574.pcf8574 import PCF8574
+import busio
+import board
 
 load_dotenv()
 
@@ -19,6 +24,42 @@ res = requests.post(auth_url, data=payload, verify=False)
 access_token = res.json()['access_token']
 print("Access Token = {}\n".format(access_token))
 
+# GPIO pins of the buttons
+S1 = 4
+S2 = 16
+S3 = 10
+S4 = 9
+inputs = [S1, S2, S3, S4]
+GPIO.setmode(GPIO.BCM)
+
+# setting up buttons as inputs
+for switch in inputs:
+    GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# set up lcd
+lcd = LCD(PCF8574(busio.I2C(board.SCL, board.SDA), 0x27), num_rows=4, num_cols=20)
+
+column = 0
+
+
+def checkSwitches():
+    # Check status of all four switches on the LCD board
+    val1 = not GPIO.input(S1)
+    val2 = not GPIO.input(S2)
+    val3 = not GPIO.input(S3)
+    val4 = not GPIO.input(S4)
+
+    if val1 == GPIO.HIGH:
+        return "S1"
+    elif val2 == GPIO.HIGH:
+        return "S2"
+    elif val3 == GPIO.HIGH:
+        return "S3"
+    elif val4 == GPIO.HIGH:
+        return "S4"
+    return "0"
+
+
 def summarize_distance_in_list(activities):
     summarized_distance = 0
 
@@ -28,7 +69,7 @@ def summarize_distance_in_list(activities):
     return summarized_distance
 
 
-def get_all_distance():
+def get_alltime_distance():
     page = 1
     page_not_empty = True
     overall_distance = 0
@@ -66,9 +107,9 @@ def get_weekly_distance():
     return summarize_distance_in_list(activities)
 
 
-def main():
-    print(get_weekly_distance())
+try:
+    lcd.print(str(get_alltime_distance() / 1000) + ' Km')
 
-
-if __name__ == "__main__":
-    main()
+except KeyboardInterrupt:
+    lcd.clear()
+    GPIO.cleanup()
