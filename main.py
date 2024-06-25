edd1c8966b2395b1cda3f1b18702b880c214b551
Justin Ceronio <justinceronio@outlook.com>
2024-06-25 19:08:22 +0200

    feat: implement progress bar view

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from PCF8574.lcd import LCD
 from PCF8574.pcf8574 import PCF8574
 import busio
 import board
+import time as stime
 
 load_dotenv()
 
@@ -39,10 +40,76 @@ for switch in inputs:
 # set up lcd
 lcd = LCD(PCF8574(busio.I2C(board.SCL, board.SDA), 0x27), num_rows=4, num_cols=20)
 
-column = 0
+zero = (
+    0b11111,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b11111
+)
+one = (
+    0b11111,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b11111
+)
+two = (
+    0b11111,
+    0b11000,
+    0b11000,
+    0b11000,
+    0b11000,
+    0b11000,
+    0b11000,
+    0b11111
+)
+three = (
+    0b11111,
+    0b11100,
+    0b11100,
+    0b11100,
+    0b11100,
+    0b11100,
+    0b11100,
+    0b11111
+)
+four = (
+    0b11111,
+    0b11110,
+    0b11110,
+    0b11110,
+    0b11110,
+    0b11110,
+    0b11110,
+    0b11111
+)
+five = (
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111
+)
+
+lcd.create_char(0, zero)
+lcd.create_char(1, one)
+lcd.create_char(2, two)
+lcd.create_char(3, three)
+lcd.create_char(4, four)
+lcd.create_char(5, five)
 
 
-def checkSwitches():
+def check_switches():
     # Check status of all four switches on the LCD board
     val1 = not GPIO.input(S1)
     val2 = not GPIO.input(S2)
@@ -86,7 +153,32 @@ def get_alltime_distance():
     for page in activity_pages:
         overall_distance += summarize_distance_in_list(page)
 
-    return overall_distance
+    return round(overall_distance)
+
+
+def update_progress_bar(count, total_count, line_to_print_on):
+    if line_to_print_on < 0 or line_to_print_on > 3:
+        raise ValueError("line_to_print_on should be in the range 0-3")
+
+    factor = total_count / 100.0  # Total count is spread over 100 characters (4 lines of 20 characters each)
+    percent = count / factor
+    number = int(percent / 5)  # Number of full blocks to print
+    remainder = int(percent % 5)  # Remainder value to print
+
+    # Print full blocks
+    for j in range(number):
+        lcd.set_cursor_pos(line_to_print_on, j)
+        lcd.write(5)  # Assuming 5 is the full block character
+
+    # Print remainder block
+    if number < 20:
+        lcd.set_cursor_pos(line_to_print_on, number)
+        lcd.write(remainder)  # Print the remainder value
+
+    # Clear the rest of the line
+    for j in range(number + 1, 20):
+        lcd.set_cursor_pos(line_to_print_on, j)
+        lcd.write(0)  # Assuming 0 is the empty block character
 
 
 def get_weekly_distance():
@@ -100,15 +192,34 @@ def get_weekly_distance():
         )
 
     header = {'Authorization': 'Bearer ' + access_token}
-    param = {'after': last_monday, 'per_page': 200, 'page': 1}  # todo: add weekly parameter
+    param = {'after': last_monday, 'per_page': 200, 'page': 1}
 
     activities = requests.get(activities_url, headers=header, params=param).json()
 
     return summarize_distance_in_list(activities)
 
 
+def display_progress():
+    lcd.clear()
+    update_progress_bar(get_weekly_distance() / 1000, 10, 0)
+    lcd.set_cursor_pos(2, 0)
+    lcd.print(str(get_weekly_distance() / 1000) + '/10 Kilometers')
+    percentage = 10 * get_weekly_distance() / 1000
+    lcd.set_cursor_pos(3, 0)
+    lcd.print(f'{percentage}% of your goal')
+
+
 try:
-    lcd.print(str(get_alltime_distance() / 1000) + ' Km')
+    display_progress()
+
+    while True:
+        mode = check_switches()
+        if mode == 'S3':
+            lcd.clear()
+            lcd.print('Alltime distance ' + str(get_weekly_distance() / 1000) + ' Kilometers')
+        if mode == 'S4':
+            display_progress()
+        stime.sleep(1)
 
 except KeyboardInterrupt:
     lcd.clear()
